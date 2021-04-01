e5f77fff959ff41e210d532dca5619090897a4a8
Stefan Joachimsthaler <sjoachimsthaler@users.noreply.github.com>
2021-04-01 23:55:00 +0200

    Kostal Wechselrichter abfragen

--- WebApplication2/Controllers/SolarApiController.cs
+++ WebApplication2/Controllers/SolarApiController.cs
@@ -4,8 +4,11 @@ using Microsoft.Extensions.Logging;
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 using WebApplication2.Model;
 
@@ -19,10 +22,12 @@ namespace WebApplication2.Controllers
     public class SolarApiController : ControllerBase
     {
         public ILogger<SolarApiController> Logger { get; }
+        public HttpClient HttpClient { get; }
 
-        public SolarApiController(ILogger<SolarApiController> logger)
+        public SolarApiController(ILogger<SolarApiController> logger, HttpClient httpClient)
         {
             Logger = logger;
+            HttpClient = httpClient;
         }
 
         [HttpGet]
@@ -131,12 +136,33 @@ namespace WebApplication2.Controllers
 
         [HttpGet]
         [Route("GetInverterRealtimeData.cgi")]
-        public CommonInverterData GetInverterRealtimeData(int deviceId)
+        public async Task<CommonInverterData> GetInverterRealtimeDataAsync(int deviceId = 1)
         {
             //var deviceId = Request.Query["deviceId"];
             // TODO request hier
             Logger.LogInformation("GET: GetInverterRealtimeData");
+            KostalMeasurements inverterData = null;
+            string stringResponse = "";
+            Dictionary<string, rootDeviceMeasurement> measurements = new Dictionary<string, rootDeviceMeasurement>();
 
+            if (deviceId == 2)
+            {
+                stringResponse = await HttpClient.GetStringAsync("http://192.168.178.29/measurements.xml");
+            }
+            else if (deviceId == 1)
+            {
+                stringResponse = await HttpClient.GetStringAsync("http://192.168.178.31/measurements.xml");
+            }
+            if (!string.IsNullOrEmpty(stringResponse))
+            {
+                XmlSerializer xmlResponse = new XmlSerializer(typeof(KostalMeasurements));
+                using (var streamReader = new StringReader(stringResponse))
+                {
+                    inverterData = (KostalMeasurements)xmlResponse.Deserialize(streamReader);
+                }
+
+                measurements = inverterData.Device.Measurements.ToDictionary(x => x.Type);
+            }
             
             CommonInverterData data = new CommonInverterData()
             {
@@ -156,22 +182,22 @@ namespace WebApplication2.Controllers
                         FrequencyAC = new ValueWithUnit()
                         {
                             Unit = "Hz",
-                            Value = 50.0
+                            Value = measurements["AC_Frequency"].ValueSpecified ? (double)measurements["AC_Frequency"].Value : 50
                         },
                         IAC = new ValueWithUnit()
                         {
                             Unit = "A",
-                            Value = 0
+                            Value = measurements["AC_Current"].ValueSpecified ? (double)measurements["AC_Current"].Value : 0
                         },
                         IDC = new ValueWithUnit()
                         {
                             Unit = "A",
-                            Value = 0
+                            Value = measurements["DC_Current1"].ValueSpecified ? (double)measurements["DC_Current1"].Value : 0
                         },
                         PAC = new ValueWithUnit()
                         {
                             Unit = "W",
-                            Value = 0
+                            Value = measurements["AC_Power"].ValueSpecified ? (double)measurements["AC_Power"].Value : 0
                         },
                         TOTAL_ENERGY = new ValueWithUnit()
                         {
@@ -181,12 +207,12 @@ namespace WebApplication2.Controllers
                         UAC = new ValueWithUnit()
                         {
                             Unit = "V",
-                            Value = 230
+                            Value = measurements["AC_Voltage"].ValueSpecified ? (double)measurements["AC_Voltage"].Value : 230
                         },
                         UDC = new ValueWithUnit()
                         {
                             Unit = "V",
-                            Value = 0
+                            Value = measurements["DC_Voltage1"].ValueSpecified ? (double)measurements["DC_Voltage1"].Value : 0
                         },
                         YEAR_ENERGY = new ValueWithUnit()
                         {
@@ -203,14 +229,6 @@ namespace WebApplication2.Controllers
                 }
             };
 
-            if (deviceId == 1)
-            {
-                data.Body.Data.PAC.Value = 0;
-            }
-            else if (deviceId == 2)
-            {
-                data.Body.Data.PAC.Value = 0;
-            }
             return data;
         }
 
---
+++ WebApplication2/Model/KostalMeasurements.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Model
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRoot("root")]
+    public partial class KostalMeasurements
+    {
+
+        private rootDiv divField;
+
+        private rootDevice deviceField;
+
+        private byte slickuniqueidField;
+
+        /// <remarks/>
+        public rootDiv div
+        {
+            get
+            {
+                return this.divField;
+            }
+            set
+            {
+                this.divField = value;
+            }
+        }
+
+        /// <remarks/>
+        public rootDevice Device
+        {
+            get
+            {
+                return this.deviceField;
+            }
+            set
+            {
+                this.deviceField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute("slick-uniqueid")]
+        public byte slickuniqueid
+        {
+            get
+            {
+                return this.slickuniqueidField;
+            }
+            set
+            {
+                this.slickuniqueidField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootDiv
+    {
+
+        private rootDivA aField;
+
+        /// <remarks/>
+        public rootDivA a
+        {
+            get
+            {
+                return this.aField;
+            }
+            set
+            {
+                this.aField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootDivA
+    {
+
+        private string idField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootDevice
+    {
+
+        private rootDeviceMeasurement[] measurementsField;
+
+        private string nameField;
+
+        private string typeField;
+
+        private string platformField;
+
+        private string hmiPlatformField;
+
+        private ushort nominalPowerField;
+
+        private string userPowerLimitField;
+
+        private string countryPowerLimitField;
+
+        private string serialField;
+
+        private uint oEMSerialField;
+
+        private byte busAddressField;
+
+        private string netBiosNameField;
+
+        private string webPortalField;
+
+        private string manufacturerURLField;
+
+        private string ipAddressField;
+
+        private System.DateTime dateTimeField;
+
+        private ushort milliSecondsField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Measurement", IsNullable = false)]
+        public rootDeviceMeasurement[] Measurements
+        {
+            get
+            {
+                return this.measurementsField;
+            }
+            set
+            {
+                this.measurementsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Platform
+        {
+            get
+            {
+                return this.platformField;
+            }
+            set
+            {
+                this.platformField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string HmiPlatform
+        {
+            get
+            {
+                return this.hmiPlatformField;
+            }
+            set
+            {
+                this.hmiPlatformField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public ushort NominalPower
+        {
+            get
+            {
+                return this.nominalPowerField;
+            }
+            set
+            {
+                this.nominalPowerField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string UserPowerLimit
+        {
+            get
+            {
+                return this.userPowerLimitField;
+            }
+            set
+            {
+                this.userPowerLimitField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string CountryPowerLimit
+        {
+            get
+            {
+                return this.countryPowerLimitField;
+            }
+            set
+            {
+                this.countryPowerLimitField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Serial
+        {
+            get
+            {
+                return this.serialField;
+            }
+            set
+            {
+                this.serialField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public uint OEMSerial
+        {
+            get
+            {
+                return this.oEMSerialField;
+            }
+            set
+            {
+                this.oEMSerialField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte BusAddress
+        {
+            get
+            {
+                return this.busAddressField;
+            }
+            set
+            {
+                this.busAddressField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string NetBiosName
+        {
+            get
+            {
+                return this.netBiosNameField;
+            }
+            set
+            {
+                this.netBiosNameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string WebPortal
+        {
+            get
+            {
+                return this.webPortalField;
+            }
+            set
+            {
+                this.webPortalField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string ManufacturerURL
+        {
+            get
+            {
+                return this.manufacturerURLField;
+            }
+            set
+            {
+                this.manufacturerURLField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string IpAddress
+        {
+            get
+            {
+                return this.ipAddressField;
+            }
+            set
+            {
+                this.ipAddressField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public System.DateTime DateTime
+        {
+            get
+            {
+                return this.dateTimeField;
+            }
+            set
+            {
+                this.dateTimeField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public ushort MilliSeconds
+        {
+            get
+            {
+                return this.milliSecondsField;
+            }
+            set
+            {
+                this.milliSecondsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rootDeviceMeasurement
+    {
+
+        private decimal valueField;
+
+        private bool valueFieldSpecified;
+
+        private string unitField;
+
+        private string typeField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool ValueSpecified
+        {
+            get
+            {
+                return this.valueFieldSpecified;
+            }
+            set
+            {
+                this.valueFieldSpecified = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Unit
+        {
+            get
+            {
+                return this.unitField;
+            }
+            set
+            {
+                this.unitField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Type
+        {
+            get
+            {
+                return this.typeField;
+            }
+            set
+            {
+                this.typeField = value;
+            }
+        }
+    }
+
+
+}
